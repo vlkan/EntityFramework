3e3e46c3491ddc05e1a96963190813279a8cc39c
Volkan Önder <thkvolkanonder@gmail.com>
2021-08-19 22:44:11 +0300

    new standarts

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product Added";
+        public static string ProductNameInvalid = "Product Name Invalid";
+        public static string MaintenenceTime = "System in Maintenance";
+        public static string ProductsListed = "Products Listed";
+    }
+}
