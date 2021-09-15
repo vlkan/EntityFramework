d8ab19543017db433df692df62a974d01a3567cb
Volkan Önder <thkvolkanonder@gmail.com>
2021-09-15 17:26:45 +0300

    fix libs

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
 using System.Text;
@@ -15,5 +16,11 @@ namespace Business.Constants
         public static string SameNameError = "Please change the name. This product already exist.";
         public static string CategoryLimitExceded = "Category Limit Exceded";
         public static string AuthorizationDenied = "No Auth";
+        internal static string UserRegistered;
+        internal static User UserNotFound;
+        internal static User PasswordError;
+        internal static string SuccessfulLogin;
+        internal static string UserAlreadyExists;
+        internal static string AccessTokenCreated;
     }
 }
