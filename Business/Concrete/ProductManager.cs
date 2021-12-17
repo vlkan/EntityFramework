1e6a6c279a1642ae08c54b12b45fd7016dcc1d99
vlkan <thkvolkanonder@gmail.com>
2021-12-17 21:27:09 +0300

    SecuredOperations active

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -32,7 +32,7 @@ namespace Business.Concrete
             this._categoryService = categoryService;
         }
 
-        //[SecuredOperation("product.add, admin")]
+        [SecuredOperation("product.add, admin")]
         [ValidationAspect(typeof(ProductValidator))] //validation
         [CacheRemoveAspect("IProductervice.Get")]
         public IResult Add(Product product)
